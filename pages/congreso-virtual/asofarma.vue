39ff131d87e192e50dbc4c1aa290dbb470e36938
erickmv <erickadrianmendezvillalpando@gmail.com>
2020-10-22 16:20:03 -0500

    asofarma

--- pages/congreso-virtual/asofarma.vue
+++ pages/congreso-virtual/asofarma.vue
@@ -11,7 +11,7 @@
                            	<p class="pl-4">Presione en donde vea el símbolo <img src="~/assets/img/buttons/SPOT.png" width="px" class="float-right pr-4 mr-3"><br>para acceder al contenido</p>
                         </div>
                         <div class="col-xl-8 pt-4 mt-3 mb-3">
-                             <img src="~/assets/images/Logo-Pfizer.png" width="300px" class=" float-left ml-2 pl-4 pt-3">
+                             <img src="~/assets/images/asofarma/logo.png" width="210px" class=" float-left ml-2 pl-4 pt-3">
                         </div>
                         <div class="col-xl-12 pl-4 registrarte">
                             <nuxt-link to="/congreso-virtual" class="btn mb-5 mb-md-3 mt-0 ml-2">Regresar</nuxt-link>
@@ -24,22 +24,13 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xl-12 p-0">
-                    <div style="position:relative"><img id="main-image" src="~/assets/images/pfizer_stand.jpg" width="100%">
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:5.0%; height:9.2%; left:60.0%; top:63.2%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:5.7%; height:9.4%; left:59.6%; top:51.8%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:3.5%; height:9.4%; left:55.3%; top:63.4%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:3.5%; height:9.4%; left:55.3%; top:52.0%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:3.3%; height:9.2%; left:49.7%; top:51.8%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:3.7%; height:9.9%; left:49.5%; top:63.2%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:3.2%; height:9.4%; left:44.3%; top:63.4%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:5.4%; height:9.9%; left:43.2%; top:51.7%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:8.4%; height:9.1%; left:67.4%; top:53.6%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:4.4%; height:5.8%; left:76.1%; top:64.0%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:10.7%; height:39.9%; left:86.8%; top:48.7%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:6.1%; height:7.6%; left:30.2%; top:69.6%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('tres')" class="rim-hotspot" style="width:9.3%; height:10.2%; left:30.1%; top:49.0%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('dos')" class="rim-hotspot" style="width:6.4%; height:9.9%; left:17.5%; top:61.9%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
-                        <a @click="$bvModal.show('uno')" class="rim-hotspot" style="width:11.6%; height:42.6%; left:-0.1%; top:46.0%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
+                    <div style="position:relative"><img id="main-image" src="~/assets/images/asofarma/stand.jpg" width="100%">
+                        <a @click="$bvModal.show('seis')" class="rim-hotspot" style="width:8.3%; height:22.1%; left:73.4%; top:56.6%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
+                        <a @click="$bvModal.show('cinco')" class="rim-hotspot" style="width:6.6%; height:11.7%; left:65.7%; top:57.9%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
+                        <a @click="$bvModal.show('cuatro')" class="rim-hotspot" style="width:6.6%; height:26.7%; left:57.0%; top:58.3%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
+                        <a @click="$bvModal.show('tres')" class="rim-hotspot" style="width:9.9%; height:13.4%; left:45.9%; top:57.1%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
+                        <a @click="$bvModal.show('dos')" class="rim-hotspot" style="width:10.7%; height:8.6%; left:24.1%; top:58.3%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
+                        <a @click="$bvModal.show('uno')"  class="rim-hotspot" style="width:13.7%; height:54.3%; left:4.7%; top:38.9%; position:absolute; cursor:pointer; display:block; z-index:5; overflow:hidden;"></a>
                     </div>
                 </div>
             </div>
@@ -51,7 +42,7 @@
                     <div class="row">
                         <div class="col-xl-6 mb-0 contacto">
                             <div class="col-xl-10 offset-xl-1 p-4">
-                                <h2 class="text-center mb-4">Contacto con ASOFARMA</h2>
+                                <h2 class="text-center mb-4 text-white">Contacto con ASOFARMA</h2>
                                 <form class="form-contact contact_form" @submit.prevent="sendContact">
                                     <div class="row">
                                         <div class="col-sm-12">
@@ -96,28 +87,45 @@
 
                             </div>
                         </div>
-                        <div class="col-xl-6 p-0 m-0" style="background-image:url(/images/pfizer/POSTER1.jpg);background-size:cover">
-                            <div class="col-xl-3 link text-center"><a href="www.google.com" target="_blank"><h3>www.Pfizer</h3></a></div>
+                        <div class="col-xl-6 p-0 m-0" style=" background-color: #E11D24 !important;">
+                            <div class="col-xl-3 link text-center"><a href="http://www.asofarma.com.mx" target="_blank"><h3>Visite nuestro sitio</h3></a></div>
                         </div>
                         <div>
                             <b-modal ref="uno" id="uno" hide-footer>
                                 <div class="d-block text-center">
-                                    <img src="~/assets/images/pfizer/Roll-up-CDE.jpg"  width="100%;">
+                                    <img src="~/assets/images/asofarma/ARTE-2_LOGOS_150dpi.jpg"  width="100%;">
                                 </div>
-                                <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
                             </b-modal>
 
                             <b-modal  id="dos" hide-footer>
                                 <div class="d-block text-center">
-                                    <img src="~/assets/images/pfizer/Roll-up-CDE.jpg"  width="100%;">
+                                   <h2 class="p-4">Pónganse en contacto con el Dr. Rodrigo Moreno / Gerente Médico Líneas Gineco y Neuro de Asofarma.</h2>
                                 </div>
-                                <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
+                                
                             </b-modal>
                             <b-modal  id="tres" hide-footer>
                                 <div class="d-block text-center">
-                                    <img src="~/assets/images/pfizer/Roll-up-CDE.jpg"  width="100%;">
+                                    <img src="~/assets/images/asofarma/ARTE-3-Izquierdo_DEMUS_150dpi.jpg"  width="100%;">
                                 </div>
-                                <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
+                                
+                            </b-modal>
+                            <b-modal  id="cuatro" hide-footer>
+                                <div class="d-block text-center">
+                                    <img src="~/assets/images/asofarma/ARTE-5_FAM-TAFIROL_AZUL_150dpi.jpg"  width="100%;">
+                                </div>
+                                
+                            </b-modal>
+                            <b-modal  id="cinco" hide-footer>
+                                <div class="d-block text-center">
+                                    <img src="~/assets/images/asofarma/ARTE-3-Derecho_NOVOVARTALON_150dpi.jpg"  width="100%;">
+                                </div>
+                                
+                            </b-modal>
+                            <b-modal  id="seis" hide-footer>
+                                <div class="d-block text-center">
+                                    <img src="~/assets/images/asofarma/ARTE-4_XUMER-60-LANZA_150dpi.jpg"  width="100%;">
+                                </div>
+                                
                             </b-modal>
 
                         </div>
@@ -130,7 +138,7 @@
 <script>
   export default {
     name: "contacto",
-    middleware: ['authenticated'],
+   middleware: ['authenticated'],
 
     data() {
       return {
@@ -156,14 +164,15 @@
         async sendContact() {
             this.disabled = true;
 
-            if (this.contact.type === 'Proveedor') {
+            /*if (this.contact.type === 'Proveedor') {
             this.contact.mail_to = 'jorge@srcongress.mx';
             } else if (this.contact.type === 'Profesor') {
             this.contact.mail_to = 'adriana.sanchez@srcongress.mx';
             } else {
             this.contact.mail_to = 'atencion@srcongress.mx';
-            }
-
+            }*/
+            
+            this.contact.mail_to = 'informacionmedica@asofarma.com.mx';
             try {
             await this.$axios.$post('/emails/contact', this.contact);
 
--- pages/congreso-virtual/index.vue
+++ pages/congreso-virtual/index.vue
@@ -62,7 +62,7 @@
 <script>
   export default {
     name: "index",
-    middleware: ['authenticated']
+    /*middleware: ['authenticated']*/
   }
 </script>
 
